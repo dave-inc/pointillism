78cf34c8e5e5044ea71fadfc2c8793ddfc916286
Trevor Grayson <trevor@trevorgrayson.com>
2021-03-28 22:05:55 -0700

    repos_over_days

--- prmonster/crm/graph/population.py
+++ prmonster/crm/graph/population.py
@@ -1,13 +1,13 @@
+from os import environ
 from seaborn import lineplot
 import matplotlib.pyplot as plt
 from prmonster.crm import repos_over_days, Connection
 
-OUTPUT_FILE = 'data/repos_over_days.png'
+OUTPUT_FILE = environ.get('POPULATION_IMG', 'data/repos_over_days.png')
 CONN = Connection()
 
 def plot_repos_over_days():
     data = repos_over_days()
-
     x, y = zip(*data)
     lineplot(x=x, y=y)
     plt.savefig(OUTPUT_FILE)
--- prmonster/crm/sql/repos_over_days.sql
+++ prmonster/crm/sql/repos_over_days.sql
@@ -2,6 +2,6 @@ SELECT
     DATE(created),
     COUNT(id)
 FROM repos
-WHERE
-    created > date('now','-14 days')
+-- WHERE created > date('now','-14 days')
+GROUP BY DATE(created)
 ORDER BY created DESC
